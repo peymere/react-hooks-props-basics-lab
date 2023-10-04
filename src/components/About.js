0da4d76d8fde72b88c42f748c5d91dab0bbbfb16
Peyton Meredith <peymeredith@gmail.com>
2023-10-03 19:21:45 -0500

    props basics lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,32 @@
 import React from "react";
+import Links from "./Links"
 
-function About() {
+function HasBio(props){
+  return (
+    <p>{props.bio}</p>
+  )
+}
+
+function NoBio(props){
+  return null
+}
+
+
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <HasBio bio={props.bio} /> : <NoBio /> }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
-}
+  }
+
+
+
+
 
 export default About;
